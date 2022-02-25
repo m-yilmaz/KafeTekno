b96bc07940d9bdfc61d69bdd66768d378db7d447
m-yilmaz <yilmazmustafa93@gmail.com>
2022-02-25 17:29:43 +0300

    program kapanıp açıldığında dolu masaların gelmesi eklendi

--- KafeTekno.UI/AnaForm.cs
+++ KafeTekno.UI/AnaForm.cs
@@ -1,10 +1,12 @@
 ﻿using KafeTekno.DATA;
 using KafeTekno.UI.Properties;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +16,11 @@ namespace KafeTekno.UI
 {
     public partial class AnaForm : Form
     {
-        KafeVeri db = new KafeVeri();
+        KafeVeri db;
         public AnaForm()
         {
             InitializeComponent();
+            VerileriOku();
             OrnekUrunleriYukle();
             MasalariOlustur();
         }
@@ -29,7 +32,7 @@ namespace KafeTekno.UI
             for (int i = 1; i <= db.MasaAdet; i++)
             {
                 ListViewItem lvi = new ListViewItem("Masa " + i);
-                lvi.ImageKey = "bos";
+                lvi.ImageKey = db.AktifSiparisler.Any(x => x.MasaNo == i) ? "dolu" : "bos";
                 lvi.Tag = i;
                 lvwMasalar.Items.Add(lvi);
             }
@@ -106,6 +109,30 @@ namespace KafeTekno.UI
         private void tsmiUrunler_Click(object sender, EventArgs e)
         {
             new UrunlerForm(db).ShowDialog();
+        }
+        private void VerileriOku()
+        {
+            try
+            {
+                string json = File.ReadAllText("veri.json");
+                db = JsonConvert.DeserializeObject<KafeVeri>(json);
+            }
+            catch (Exception)
+            {
+                db = new KafeVeri();
+                OrnekUrunleriYukle();
+            }
+        }
+
+        private void AnaForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            VerileriKaydet();
+        }
+
+        private void VerileriKaydet()
+        {
+            string json = JsonConvert.SerializeObject(db);
+            File.WriteAllText("veri.json", json);
         }
     }
 }
